437f6ed725e51c8b07228729249581c71042a1ad
gunthergl <gunthergl@gmx.net>
2023-10-17 12:42:50 +0200

    dataMelanoma::create_data_project("2022-Lozano-NatMed") (new version)

--- data-raw/2022-Lozano-NatMed/main.R
+++ data-raw/2022-Lozano-NatMed/main.R
@@ -1,1 +1,9 @@
 # dataMelanoma::create_data_project("2022-Lozano-NatMed")
+proj_dir <- "data-raw/2022-Lozano-NatMed/"
+
+
+#### 1. Download data
+download.file(
+    url = "INSERT_URL_HERE",
+    destfile = file.path(proj_dir, "data")
+)
