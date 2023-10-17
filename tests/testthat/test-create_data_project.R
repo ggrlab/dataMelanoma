3d3221e9a34782cd7be06cbe778134c7b5cb0f99
gunthergl <gunthergl@gmx.net>
2023-10-17 11:20:34 +0200

    test create_data_project

--- tests/testthat/test-create_data_project.R
+++ tests/testthat/test-create_data_project.R
@@ -1,4 +1,4 @@
 test_that("create_data_project", {
     create_data_project("removeme-test")
-    unlink(file.path("data-raw", "removeme-test"))
+    unlink(file.path("data-raw", "removeme-test"), recursive = TRUE)
 })
