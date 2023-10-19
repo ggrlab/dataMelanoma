a18e23857f022eda8d906b4cac9bbc46350aa630
gunthergl <gunthergl@gmx.net>
2023-10-19 19:20:47 +0200

    include Stem.Sig from zhang2022

--- data-raw/2022-Zhang-GenomeMedicine/main.R
+++ data-raw/2022-Zhang-GenomeMedicine/main.R
@@ -21,10 +21,16 @@ if (!file.exists(single_example_file_from_downloaded)) {
 #### 2. Read data
 load(single_example_file_from_downloaded)
 source(file.path(proj_dir, "src", "f01_combat_tvt.R"))
+
+# Zhang et al did their machine learning comparison by first restricting
+# the data to the features in the gene signature "Stem.Sig"
+# I was unable to find how the signature was GENERATED, only how it was loaded.
+load(file.path(proj_dir, "data", "GM", "data", "sig", "Stem.Sig.Rdata"))
 #### 3. Save data in qrds format
 qs::qsave(training, file.path(proj_dir, "res", "training.qs"))
 qs::qsave(validation, file.path(proj_dir, "res", "validation.qs"))
 qs::qsave(test, file.path(proj_dir, "res", "test.qs"))
+qs::qsave(Stem.Sig, file.path(proj_dir, "res", "Stem.Sig.qs"))
 
 
 
@@ -37,3 +43,6 @@ usethis::use_data(zhang2022_validation, overwrite = TRUE)
 
 zhang2022_test <- test
 usethis::use_data(zhang2022_test, overwrite = TRUE)
+
+zhang2022_Stem.Sig <- Stem.Sig
+usethis::use_data(zhang2022_Stem.Sig, overwrite = TRUE)
